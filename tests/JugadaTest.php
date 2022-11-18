79b02e0011c93c87ccb4dbc10a8af4e3a8359763
gemapatricia <gp.herrera@usp.ceu.es>
2022-11-18 16:51:53 +0100

    NumeroTiradasStrike pasa

--- tests/JugadaTest.php
+++ tests/JugadaTest.php
@@ -4,14 +4,14 @@ use PHPUnit\Framework\TestCase;
 
 Class JugadaTest extends TestCase{
 
-    public function testNumeroTiradasEnUnaJugada(){
+    public function testNumeroTiradasEnUnaJugadaNormal(){
         $jugada= new App\Jugada(4,5);
         $this->assertEquals($jugada->getNumeroTiradas(), 2, "No incide el número de tiradas en una jugada");
     } 
 
     public function testNumeroTiradasEnUnaJugadaStrike(){
         $jugada= new App\Jugada(10);
-        $this->assertEquals($jugada->getNumeroTiradas(), 2, "No incide el número de tiradas en una jugada");
+        $this->assertEquals($jugada->getNumeroTiradas(), 1, "No incide el número de tiradas en una jugada");
     } 
 
 
