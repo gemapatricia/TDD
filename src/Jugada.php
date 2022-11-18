80e32772f2587bd66f626adab63ced85a996a8d2
gemapatricia <gp.herrera@usp.ceu.es>
2022-11-18 16:50:23 +0100

    NumeroTiradasStrike falla

--- src/Jugada.php
+++ src/Jugada.php
@@ -7,8 +7,13 @@ Class Jugada{
     private $numeroTiradas;
     private $puntuacionJugada;
 
-    public function __construct($tirada1, $tirada2){
-        $this->numeroTiradas = 2;
+    public function __construct($tirada1, $tirada2=0){
+        if ($tirada2 == 0){
+            $this->numeroTiradas = 1;
+        }
+        else{
+            $this->numeroTiradas = 2;
+        }
         $this->puntuacionJugada = $tirada1 + $tirada2;
     }
 
