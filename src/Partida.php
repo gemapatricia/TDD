be3f35586b6621f46b642dae1d44a5287928e70a
gemapatricia <gp.herrera@usp.ceu.es>
2022-11-19 21:03:14 +0100

    Puntuacion tirada 1 falla

---
+++ src/Partida.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace App;
+
+Class Partida{
+
+    private $puntuacion = 1;
+
+    public function getPuntuacion(){
+        return $this->puntuacion;
+    }
+
+}
--- tests/PartidaTest.php
+++ tests/PartidaTest.php
@@ -5,8 +5,8 @@ use PHPUnit\Framework\TestCase;
 Class PartidaTest extends TestCase{
     
     public function testPartidaUnaTirada(){
-        $partida = new Partida("1- -- -- -- -- -- -- -- -- --");
-        $this->assertEquals($partida.getPuntuacion(), 1, "No coincide la puntuación de la tirada");
+        $partida = new App\Partida("1- -- -- -- -- -- -- -- -- --");
+        $this->assertEquals($partida->getPuntuacion(), 2, "No coincide la puntuación de la tirada");
     }   
 }
 
