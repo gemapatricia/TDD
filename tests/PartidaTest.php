454c5506ad8f7ce36fa550cb7621d616d55a5156
gemapatricia <gp.herrera@usp.ceu.es>
2022-11-19 21:05:39 +0100

    Test puntuacion tirada 1 valor 6 falla

--- tests/PartidaTest.php
+++ tests/PartidaTest.php
@@ -7,7 +7,12 @@ Class PartidaTest extends TestCase{
     public function testPartidaUnaTirada(){
         $partida = new App\Partida("1- -- -- -- -- -- -- -- -- --");
         $this->assertEquals($partida->getPuntuacion(), 1, "No coincide la puntuación de la tirada");
-    }   
+    }
+    
+    public function testPartidaUnaTirada2(){
+        $partida = new App\Partida("6- -- -- -- -- -- -- -- -- --");
+        $this->assertEquals($partida->getPuntuacion(), 6, "No coincide la puntuación de la tirada");
+    }  
 }
 
 ?>
